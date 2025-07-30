800235a0890f30ab8986fc7b107d865c8988ba48
Joab Sena <joabarbosa1@hotmail.com>
2025-07-30 15:13:56 -0300

    Refactor code structure for improved readability and maintainability

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -1,6 +1,6 @@
-import { env } from "../env";
-import { app } from "./app";
+import { env } from '../env'
+import { app } from './app'
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log(`🚀 Server is running on PORT:${env.PORT}`);
+  console.log(`🚀 Server is running on PORT:${env.PORT}`)
 })
