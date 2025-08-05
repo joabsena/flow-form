e1e9810989c5aa25a8c1ee2d4af76d74771eb9c2
Joab Sena <joabarbosa1@hotmail.com>
2025-08-05 09:00:00 -0300

    feat: integrate fastify-type-provider-zod and add Swagger documentation
    - Updated app.ts to include fastify-type-provider-zod for enhanced type safety with Zod schemas.
    - Configured Swagger documentation with fastifySwagger and fastifySwaggerUI for API documentation.
    - Refactored usersRouter to utilize Zod for request validation on the /users endpoint.
    - Added necessary dependencies for fastify-type-provider-zod, fastify-swagger, and fastify-swagger-ui in pnpm-lock.yaml.

--- packages/core/src/app.ts
+++ packages/core/src/app.ts
@@ -2,8 +2,34 @@ import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from '../env'
 import { usersRouter } from '@modules/user/router/router'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 export const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Flow Form API',
+      description: 'API for managing forms',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/documentation',
+})
 
 app.register(usersRouter)
 
--- packages/core/src/modules/user/router/router.ts
+++ packages/core/src/modules/user/router/router.ts
@@ -1,6 +1,19 @@
 import { FastifyInstance } from 'fastify'
 import { register } from '../controllers/register'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export async function usersRouter(app: FastifyInstance) {
-  app.post('/users', register)
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: 'POST',
+    url: '/users',
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+      }),
+    },
+    handler: register,
+  })
 }
